d47dc56924fd49144e927e51d55dfb67f8716c5d
Kelly Henderson <kellyjeanhenderson@gmail.com>
2023-07-05 14:25:46 -0500

    clientSide

---
+++ client-side/src/components/Camera.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import Camera, {IMAGE_TYPES, FACING_MODES} from 'react-html5-camera-photo';
+import ImagePreview from './ImagePreview';
+
+
+const Camera = (props) => {
+    const [dataUri, setDataUri] = useState('');
+
+
+    function handleTakePhotoAnimationDone (dataUri) {
+        setDataUri(dataUri);
+     
+        console.log('takePhoto' );
+    
+    
+      }
+const isFullscreen= false
+  return (
+    <>
+    {
+      (dataUri)
+        ? <div>
+            <ImagePreview dataUri={dataUri}/>
+        </div>
+  :<Camera onTakePhotoAnimationDone = {handleTakePhotoAnimationDone}
+  isFullscreen={isFullscreen}
+  imageType = {IMAGE_TYPES.JPG}
+  imageCompression = {0.97}
+  isMaxResolution = {true}
+  idealFacingMode = {FACING_MODES.ENVIRONMENT}
+  idealResolution = {{width: 390, height: 844}}
+  
+  />
+  
+}
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Camera
---
+++ client-side/src/components/ImagePreview.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types';
+import MenuReader from './MenuReader';
+
+export const ImagePreview = ({ dataUri, isFullscreen }) => {
+    let classNameFullscreen = isFullscreen ? 'demo-image-preview-fullscreen' : '';
+    const [dataUri, setDataUri] = useState({dataUri})
+    const [interpret, setInterpret] = useState(false)
+const handleRetake = () => { 
+    setDataUri("")
+    
+ }
+const handleSubmit =(dataUri)=>{
+    setInterpret(true)
+}
+
+    return (
+        <div>
+      <div className={'demo-image-preview ' + classNameFullscreen}>
+        <img src={dataUri} />
+
+        <button onClick={handleRetake}>Try again</button>
+        <button onClick={handleSubmit}>Use this pic</button>
+      </div>
+
+      <div>{
+        <div>
+        (interpret == true)?
+            <MenuReader dataUri={dataUri}/>
+        </div>
+    }:
+      </div>
+      </div>
+    );
+  };
+  
+  ImagePreview.propTypes = {
+    dataUri: PropTypes.string,
+    isFullscreen: PropTypes.bool
+  };
+  
+  export default ImagePreview;
