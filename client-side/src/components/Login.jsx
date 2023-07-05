75ec0925073794d7291c510776a1cef272a8d8a9
Katie Toney <kathryn.toney@gmail.com>
2023-07-05 15:16:06 -0400

    weds 3:15pm ET

--- client-side/src/components/Login.jsx
+++ client-side/src/components/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Box, Typography, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, Typography, TextField, Modal, Button } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import Registration from './Registration'
 
 const StyledTextField = styled(TextField)({
     display: "flex",
@@ -11,7 +12,15 @@ const StyledTextField = styled(TextField)({
     marginTop: '10px'
 })
 
+const StyledModal = styled(Modal)({
+    display: 'flex',
+    alignItems: "center",
+    justifyContent: 'center'
+})
+
 const Login = () => {
+    const [openRegistration, setOpenRegistration] = useState(false)
+
     return (
         <>
             <Typography variant='h5' sx={{ fontFamily: "'Nunito', sans - serif", display: 'flex', justifyContent: 'center', marginTop: '10px' }}>Login:</Typography>
@@ -20,6 +29,18 @@ const Login = () => {
                 <StyledTextField placeholder='password' />
             </Box>
             <Box>
+                <Typography variant='h5' sx={{ fontFamily: "'Nunito', sans - serif", display: 'flex', justifyContent: 'center', marginTop: '10px' }}><a href='#' onClick={e => setOpenRegistration(true)}>First time here?</a></Typography>
+                <StyledModal
+                    open={openRegistration}
+                    onClose={e => setOpenRegistration(false)}
+                    aria-labelledby='modal-modal-title'
+                    aria-describedby='modal-modal-description'
+                >
+                    <Box width={400} height={350} bgcolor='white' p={3} borderRadius={5}>
+                        <Typography variant='h6' textAlign='center'>Register Now:</Typography>
+                        <Registration />
+                    </Box>
+                </StyledModal>
             </Box>
         </>
     )
